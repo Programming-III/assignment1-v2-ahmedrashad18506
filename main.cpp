ac1af96bc0763c6fd8ff71b0336d9a9a751593b1
ahmedrashad18506 <ahmed.rashad@student.giu-uni.de>
2025-11-11 09:52:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,10 @@
 using namespace std;
 
 // ==================== Person Class Implementation =========================
-
+Person :: display(){
+        cout<< "Major: "major << endl;
+        cout<< "Year: "yearLevel << endl;
+    }
 
 
 
@@ -18,14 +21,20 @@ using namespace std;
 
 
 // ==================== Student Class Implementation ====================
-
+Student :: display(){
+        cout<< "Major: "major << endl;
+        cout<< "Year: "yearLevel << endl;
+    }
 
 
 
 
 
 // ==================== Instructor Class Implementation ====================
-
+Instructor :: display(){
+        cout<< "Department: "department << endl;
+        cout<< "Experience: "exYears << endl;
+    }
 
 
 
@@ -33,8 +42,22 @@ using namespace std;
 
 // ==================== Course Class Implementation ====================
 
-
-
+Course :: addStudent(const Student& s){
+       s = Student* student;
+    }
+Course :: displayCourseInfo{
+        cout<< "Course: " courseCode "-" courseName<<endl;
+        cout<<"Max students:" maxStudents <<endl;
+        cout<< "Currently enrolled:" Student.name "(ID:" Student.id ")" <<endl;
+        cout<<"Instructor Info:" << endl;
+        cout<<"Name: " Instructor.name <<endl;
+        cout<<"Department: " Instructor.department<<endl;
+        cout<<"Experiencxe: " Instructor.exYears<<endl;
+        cout<<"Student Info:" << endl;
+        cout<< "Year: " Student.yearLevel << endl;
+        cout<< "Major: " Student.major << endl;
+        
+    }
 
 
 
@@ -43,6 +66,15 @@ using namespace std;
 // ==================== Main Function ====================
 int main() {
    
+    std::cout<<"Hello World";
+    Student s = new Student();
+    Instructor i = new Instructor();
+    Course c = new Course();
+   Student* s2 = new Student()l;
+   c.addStudent(Student* s2);
+   c.displayCousreInfo():
+      delete s2;
+    
     
     return 0;
 }
